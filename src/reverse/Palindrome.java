57d4bffb2beb202775a820928fa912be11f8b919
varshithahify <varshithavn26@gmail.com>
2026-02-17 19:59:48 +0530

    updated palindrome and reverse string programs

---
+++ src/reverse/Palindrome.java
@@ -0,0 +1,19 @@
+package reverse;
+
+//program to check wheather the given string is pallindrome or not
+public class Palindrome {
+	public static void main(String[] args) {
+		  String str="mom";
+		  String res=" ";
+		  System.out.println(str);
+		  for(int i=str.length()-1;i>=0;i--) {
+			  res = res + str.charAt(i);
+		  }
+		  if(str.equals(res)) {
+			  System.out.println("it is pallindrome");
+		  }
+		  else {
+			  System.out.println("it's not pallindrome");
+		  }
+	}}
+
