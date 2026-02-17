9063166392bd9ade265380c0ab2d3b4809c3789a
varshithahify <varshithavn26@gmail.com>
2026-02-17 20:35:03 +0530

    changed days

---
+++ src/reverse/Day2_ReverseString.java
@@ -0,0 +1,17 @@
+package reverse;
+
+//Reverse the given string
+public class ReverseString {
+		public static void main(String[] args) {
+	
+	      String s = "java";
+	      String res = "";
+	
+	      for (int i = s.length() - 1; i >= 0; i--) {
+	          res = res + s.charAt(i);
+	      }
+	
+	      System.out.println(res);
+	  }
+	}
+
