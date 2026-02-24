d39f312b25c1ce6c491ccca2551e2ab91aeca097
varshithahify <varshithavn26@gmail.com>
2026-02-24 20:20:55 +0530

    added program to print even index elements

---
+++ src/reverse/EvenIndex.java
@@ -0,0 +1,14 @@
+package reverse;
+
+//print only even index elements in array
+public class EvenIndex {
+	public static void main(String[] args) {
+			int[] a= {1,2,3,4,5,6};
+			for(int i=0;i<a.length;i++) {
+				if(i%2==0) {
+					System.out.println(a[i]);
+				}
+			}
+	 	}
+	}
+
