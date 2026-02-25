ca55c4d17bc65fb211321fd7fc775bf086b87379
varshithahify <varshithavn26@gmail.com>
2026-02-25 22:36:40 +0530

    Added program to print the sum of each number

---
+++ src/reverse/Day10_SumEachNumber.java
@@ -0,0 +1,15 @@
+package reverse;
+
+//print the sum of each number
+public class Day10_SumEachNumber {
+		public static void main(String[] args) {
+			int[] a= {1,2,3,4,5,6};
+			int sum=0;
+			for(int i=0;i<a.length;i++) {
+				sum=sum+a[i];
+				
+			}
+			System.out.println(sum);
+		}
+	}
+
