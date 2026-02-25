6114f9de5974f6b3045811f62395332ad36aa11b
varshithahify <varshithavn26@gmail.com>
2026-02-25 22:32:56 +0530

    added new edited prograam

--- src/reverse/Day9_EvenIndex.java
+++ src/reverse/Day9_EvenIndex.java
@@ -1,7 +1,7 @@
 package reverse;
 
 //print only even index elements in array
-public class EvenIndex {
+public class Day9_EvenIndex {
 	public static void main(String[] args) {
 			int[] a= {1,2,3,4,5,6};
 			for(int i=0;i<a.length;i++) {
