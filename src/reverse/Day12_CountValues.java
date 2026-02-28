8c1328d8e0775fd99c945a5c5a3f455a4f312978
varshithahify <varshithavn26@gmail.com>
2026-02-28 22:12:30 +0530

    Added program to count of values in the array

---
+++ src/reverse/Day12_CountValues.java
@@ -0,0 +1,14 @@
+package reverse;
+
+//count of values in the array
+public class Day12_CountValues {
+		public static void main(String[] args) {
+			int[] a= {1,2,3,4,5,6};
+			int count=0;
+			for(int i=0;i<a.length;i++) {
+				count++;
+			}
+			System.out.println(count);
+			}
+		}
+
