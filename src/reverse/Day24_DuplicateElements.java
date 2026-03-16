0d24c65b18a6d6d1cc942fbdea39575aa8b05a55
varshithahify <varshithavn26@gmail.com>
2026-03-16 21:46:23 +0530

    added program to print unique elements in the given array

---
+++ src/reverse/Day24_DuplicateElements.java
@@ -0,0 +1,22 @@
+package reverse;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+//program to print unique elements in the given array
+public class Day24_DuplicateElements {
+		public static void main(String[] args) {
+			int[] a= {1,2,2,3,3,3,4,4,5,6};
+			Map<Integer,Integer> m=new LinkedHashMap<Integer, Integer>();
+			for(int i=0;i<a.length;i++) {
+				m.put(a[i], m.getOrDefault(a[i],0)+1);
+			}
+			System.out.println("key");
+			for(Entry<Integer, Integer> l:m.entrySet()) {
+				if(l.getValue()==1)
+				System.out.println(l.getKey());
+			}
+		}
+	}
+
